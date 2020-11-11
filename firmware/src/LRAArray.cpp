48c67fff5e7155fd11aadff9c9dd590d85b2010e
Dominic Canare <dom@greenlightgo.org>
2020-11-10 22:19:10 -0600

    Fast calibrate

--- firmware/src/LRAArray.cpp
+++ firmware/src/LRAArray.cpp
@@ -63,10 +63,6 @@ void LRAArray::disableMuxForActuator(uint8_t id) {
 
 void LRAArray::setValue(uint8_t id, uint8_t value) {
 	if(isOk(id)){
-//		Serial.print("Send then disable mux");
-//		Serial.print(id);
-//		Serial.print(' ');
-//		Serial.println(value);
 		sendThenDisableMux(id, value);
 	}
 }
@@ -75,7 +71,7 @@ bool LRAArray::isOk(uint8_t id){
 	return actuatorStatus[id] == HAPTIC_SUCCESS;
 }
 
-void LRAArray::calibrate(){
+void LRAArray::calibrate(bool fast){
 	Logger::getGlobal()->info("Init status");
 
 	char buffer[45] = "Init status ";
@@ -85,7 +81,6 @@ void LRAArray::calibrate(){
 			continue;
 		}
 
-		uint8_t data;
 		uint8_t thisStatus = -99;
 
 		switchTo(actuatorID);
@@ -96,7 +91,26 @@ void LRAArray::calibrate(){
 
 			driver.setActuatorType(LRA);
 			driver.playScript(0);       // Reset/Init
-			driver.writeRegScript(calibrate_mplus_0934w);
+			if(fast){
+				driver.writeRegScript(default_mplus_0934w);
+				setValue(actuatorID, 255);
+				delay(100);
+				setValue(actuatorID, 0);
+			}else{
+				driver.writeRegScript(calibrate_mplus_0934w);
+				char str[45];
+				uint8_t feedbackControl = driver.readReg(DRV2605_REG_FEEDBACK);
+				sprintf(str, "Feedback control %d", feedbackControl);
+				Logger::getGlobal()->info(str);
+
+				uint8_t calComp = driver.readReg(DRV2605_REG_AUTOCALCOMP);
+				sprintf(str, "calComp          %d", calComp);
+				Logger::getGlobal()->info(str);
+
+				uint8_t calBemf = driver.readReg(DRV2605_REG_AUTOCALEMP);
+				sprintf(str, "calBemf          %d", calBemf);
+				Logger::getGlobal()->info(str);
+			}
 			driver.setMode(INACTIVE_MODE);
 		}else{
 
@@ -112,8 +126,6 @@ void LRAArray::calibrate(){
 void LRAArray::disableAll(){
 	uint8_t id = 0;
 	for(id = 0; id+7<this->arrayCount; id+=8){
-//		Serial.print("Disable mux for ");
-//		Serial.println(id);
 		MuxedActuator muxedActuator = toMuxed(id);
 
 		Wire.beginTransmission(muxedActuator.muxAddress);
@@ -129,8 +141,6 @@ void LRAArray::disableAll(){
 	for(uint8_t i=0; i+id<this->arrayCount; i++){
 		addressBits += 1 << i;
 	}
-//	Serial.print("Disable mux for ");
-//	Serial.println(addressBits);
 	Wire.beginTransmission(lastMux.muxAddress);
 	Wire.write(addressBits);
 	Wire.endTransmission();
--- firmware/src/LRAArray.h
+++ firmware/src/LRAArray.h
@@ -13,10 +13,11 @@ typedef struct {
 	uint8_t deviceID;
 } MuxedActuator;
 
+
 class LRAArray{
 	public:
 		void setup(uint8_t arrayCount, uint8_t startMux, uint8_t startIdx);
-		void calibrate();
+		void calibrate(bool fast=true);
 		void switchTo(uint8_t id);
 		void setValue(uint8_t value);
 		void setValue(uint8_t id, uint8_t value);
@@ -57,6 +58,23 @@ const struct scr_type calibrate_mplus_0934w[] = {
 	{ACTUATOR_SCRIPT_DELAY,     0xFF}, //! DRV2605 - delay 0.25 sec
 	{ACTUATOR_SCRIPT_DELAY,     0xFF}, //! DRV2605 - delay 0.25 sec (1.25 sec max)
 	{DRV2605_REG_MODE,          0x01}, //! DRV2605 - calibrate
+	{ACTUATOR_SCRIPT_END,       0x00}  //! DRV2605 - end of script flag
+};
+
+const struct scr_type default_mplus_0934w[] = {
+	{DRV2605_REG_RATEDV,        0x51}, //! DRV2605 - rated voltage 2V
+	{DRV2605_REG_CLAMPV,        0x64}, //! DRV2605 - clamp voltage = 2.2V
+	{DRV2605_REG_FEEDBACK,      0xF6}, //! DRV2605 - LRA mode
+
+	{DRV2605_REG_CONTROL1,      0x93}, //! DRV2605 - drive_time set for 200Hz = 2.5mSec
+	{DRV2605_REG_CONTROL2,      0xF5}, //! DRV2605 - sample_time = 3, balnk =1, idiss = 1
+	{DRV2605_REG_CONTROL3,      0x88}, //! DRV2605 - LRA closed loop
+	{DRV2605_REG_CONTROL4,      0x30}, //! DRV2605 - Autocal time = 3 (1.2 seconds!)
+
+	{DRV2605_REG_LIBRARY,       0x06}, //! DRV2605 - Library 6 is LRA
+
+	{DRV2605_REG_AUTOCALCOMP,   0x0C},
+	{DRV2605_REG_AUTOCALEMP,    0x6C},
 	{ACTUATOR_SCRIPT_END,       0x00}  //! DRV2605 - end of script flag
 };
 
